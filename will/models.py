e14fa85f932fe82ba30e2207e03629f302098002
Timothy John <timjohnargota@gmail.com>
2018-10-14 01:19:39 +0800

    Added new models

--- will/models.py
+++ will/models.py
@@ -2,5 +2,28 @@ from django.db import models
 
 
 class User(models.Model):
-    first_names = models.CharField(max_length=256)
-    last_names = models.CharField(max_length=512)
+    name = models.CharField(max_length=256, blank=True)
+    gender_list = (('F','Female'),('M','Male'))
+    gender = models.CharField(
+        max_length = 1,
+        choices = gender_list,
+        blank=True
+    )
+    address = models.CharField(max_length=256, blank=True)
+    NRIC = models.CharField(max_length = 9)
+    date_of_birth = models.DateField(auto_now=True)
+
+class Main(User):
+    bankAcc = models.CharField(max_length=12)
+
+class Asset(models.Model):
+    TYPES_OF_ASSET = (('ch', 'Cash'), ('psb','Private Equity, Stock and Bonds'),
+                        ('re','Real Estate'), ('cm', 'Commodity'),
+                        ('pt','Patents'), ('fa','Furniture and Appliances'))
+    asset_type = models.CharField(
+        max_length = 2,
+        choices = TYPES_OF_ASSET,
+        blank=True
+    )
+
+
